e6277dc6d9386b4e3c62e6b805b8c3f7a2faf8bd
Vineet Chaudhari <vineetchaudhari2122@gmail.com>
2026-05-23 17:38:26 +0530

    Final production API fixes

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://study-sync-sv0t.onrender.com",
+  baseURL: "https://study-sync-sv0t.onrender.com/api",
 });
 
 export default API;
